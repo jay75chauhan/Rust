a93bb2e59cd432f9adce5237dfa7cb020f536c34
jay75chauhan <chauhan75jayk@gmail.com>
2021-11-23 18:18:01 +0530

    hellow cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\chauh\Desktop\jay learning\Rust\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\Users\chauh\Desktop\jay learning\Rust\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\chauh\Desktop\jay\ learning\Rust\hello_cargo\target\debug\hello_cargo.exe: C:\Users\chauh\Desktop\jay\ learning\Rust\hello_cargo\src\main.rs
